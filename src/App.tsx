c347c8f45f5d64780f66ff83cb7fe1edf13fed02
Peter Zaťko <zatkopeter301@gmail.com>
2023-05-22 11:53:33 +0200

    basic hello world

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import { FC } from 'react'
+import HelloWorld from './components/HelloWorld'
+import Clock from './components/Clock';
+
+const App: FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App background-img">  
+      <div className="grid h-screen place-items-center">
+        <div>
+          <h1 className="text-3xl font-bold first-letter:underline">
+            <HelloWorld /> 
+          </h1>
+          <h2 className="font-light"><Clock /></h2>
+        </div>
+      </div>
     </div>
   );
 }
